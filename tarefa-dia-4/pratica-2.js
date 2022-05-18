2e00dc11c1923f08e611ca46dba6cd0f651e9e73
heydiniz <aryasha5@hotmail.com>
2022-05-18 15:46:13 -0300

    conclusão das tarefas do dia 4 sobre funções e comandos de repetição

---
+++ tarefa-dia-4/pratica-2.js
@@ -0,0 +1,19 @@
+let tabuada = [
+    {'número' : 7*1 },
+    {'número' : 7*2 },
+    {'número' : 7*3 },
+    {'número' : 7*4 },
+    {'número' : 7*5 },
+    {'número' : 7*6 },
+    {'número' : 7*7 },
+    {'número' : 7*8 },
+    {'número' : 7*9 },
+    {'número' : 7*10 }
+
+]  
+
+    for (let contador = 0; contador <= 9; contador++) {
+  
+    console.log(tabuada[contador])
+}
+
--- tarefa-dia-4/treinando-com-a-aula.js
+++ tarefa-dia-4/treinando-com-a-aula.js
@@ -7,3 +7,18 @@ console.log("O Jensen é talentoso.")
 imprimirNome () 
 console.log("O Jensen é lindo.")
 imprimirNome ()
+
+function somar (dean, cas) {
+console.log("o primeiro nome é:" , dean)
+console.log("o segundo nome é:" , cas)
+console.log("A soma é:" , dean+cas)
+
+console.log(cas+dean)
+
+}
+somar(10, 20)
+somar(50, 40)
+
+function multiplicar (7) {
+    console.log("a tabuada do 7 é:" , ) 
+}
