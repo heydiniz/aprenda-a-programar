249d392df1bd759e42c2cb2634e1d9ae9f9878c1
heydiniz <aryasha5@hotmail.com>
2022-05-18 15:50:47 -0300

    criação do meu primeiro código e inserção de variáveis

---
+++ tarefa-dia-3/pratica-1.js
@@ -0,0 +1,16 @@
+let nome, email, profissão, idade, lugar
+
+console.log(nome,email,profissão,idade,lugar)
+
+nome = 'Ariadne'
+email = 'aryasha5_hotmail'
+profissão = 'recreadora'
+idade = '28'
+lugar = 'NovaYork'
+
+console.log({nome, email, profissão, idade, lugar})
+
+
+
+
+
